1fd33ff619eb3112a5f5e1cefe84bed3737e5031
Suresh Yhap <sureshyhap@gmail.com>
2020-04-29 21:54:21 -0400

    Finished Problem 7

--- C++/7/7.cpp
+++ C++/7/7.cpp
@@ -2,12 +2,24 @@
 #include <cmath>
 
 int main(int argc, char* argv[]) {
-  int i = 0;
+  int i = 3;
   int prime = 1;
-  while (i < 10001) {
-    ++prime;
-    for (int j = )
+  while (true) {
+    bool is_prime = true;
+    for (int j = 2; j <= sqrt(i); ++j) {
+      if (i % j == 0) {
+	is_prime = false;
+	break;
+      }
+    }
+    if (is_prime) {
+      ++prime;
+    }
+    if (prime == 10001) {
+      break;
+    }
+    ++i;
   }
-  
+  std::cout << i;
   return 0;
 }
